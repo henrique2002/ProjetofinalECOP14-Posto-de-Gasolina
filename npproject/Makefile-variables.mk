9c28bfd800cb70f5423639fb6b3f12f9f420523b
henrique2002 <94972501+henrique2002@users.noreply.github.com>
2021-12-17 17:55:22 -0300

    Add files via upload

---
+++ npproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=POSTO.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/POSTO.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=posto.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/posto.x.tar
